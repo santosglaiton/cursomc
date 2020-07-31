16b3b35be82c972d4f2a99bf2152316ce167d54b
santosglaiton <glaitonlemos@gmail.com>
2020-07-31 15:46:17 -0300

    Implementando autorização

--- src/main/java/com/santosglaiton/cursomc/config/SecurityConfig.java
+++ src/main/java/com/santosglaiton/cursomc/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.santosglaiton.cursomc.config;
 
 import com.santosglaiton.cursomc.security.JWTAuthenticationFilter;
+import com.santosglaiton.cursomc.security.JWTAuthorizationFilter;
 import com.santosglaiton.cursomc.security.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -53,6 +54,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .antMatchers(PUBLIC_MATCHERS).permitAll()
         .anyRequest().authenticated();
         http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+        http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
